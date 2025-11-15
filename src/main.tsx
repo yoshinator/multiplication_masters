35560bbf6e5858f4281e5f31924659eb2918f9b9
Yoan <yoanante@gmail.com>
2025-11-14 20:59:37 -0500

    Refactor and add timer

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
-import { FirebaseProvider } from './contexts/FirebaseProvider.tsx'
+import { FirebaseProvider } from './contexts/firebase/FirebaseProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
