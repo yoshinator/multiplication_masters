ad605774d33f7ef13d7bc2a2e4103444408537cb
Yoan <yoanante@gmail.com>
2025-12-25 14:12:27 -0500

    Save dark mode preferences to localStorage

--- src/contexts/themeContext/ThemeContextProvider.tsx
+++ src/contexts/themeContext/ThemeContextProvider.tsx
@@ -4,8 +4,9 @@ import { ThemeProvider, useMediaQuery, CssBaseline } from '@mui/material'
 import { theme, darkTheme } from '../../theme/theme'
 
 export const ThemeContextProvider = ({ children }: { children: ReactNode }) => {
-  // You can initialize this from localStorage to persist the user's choice
-  const [mode, setMode] = useState<ColorMode>('system')
+  const [mode, setMode] = useState<ColorMode>(
+    (localStorage.getItem('mode') as ColorMode) || 'system'
+  )
 
   // This hook listens to the system/OS preference
   const systemPrefersDark = useMediaQuery('(prefers-color-scheme: dark)')
